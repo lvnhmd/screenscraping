6925f7e75ad99a0b10a3cac2db3c4f9cbaa55bef
elvin <ali.elvin@gmail.com>
2015-08-06 22:49:41 +0100

    add selfridges source to the required list, improve logging and format code

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ require('colors').setTheme('../themes/generic-logging.js');
 console.log('server.js begin'.info);
 
 var asServer = new require('./as/')(
-	require('./config'), [require('./sources/live')]
+    require('./config'), [
+        require('./sources/live'),
+        require('./sources/selfridges')
+    ]
 );
 
-console.log('server.js end'.help);
+console.log('server.js end'.info);
