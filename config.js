536f003cfc2b89c31758971d97da5b25bcb24cd3
elvin <ali.elvin@gmail.com>
2015-08-06 22:47:53 +0100

    add dynamic source and cache dir

--- config.js
+++ config.js
@@ -1,11 +1,17 @@
 module.exports = {
 	port: 8080,
+	cacheDir: '../../cache',
 	// source configs
 	sourceTypes: {
 		'static': {
 			method: 'FILE',
 			basePath: 'sources/static/',
 			feed: 'static'
+		},
+		'dynamic': {
+			method: 'GET',
+			basePath: 'http://',
+			feed: 'dynamic'
 		}
 	}
 };
